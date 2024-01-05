beb37cf58337fb25c7216516384e1e79e27898d9
oofy2323 <106826946+oofy2323@users.noreply.github.com>
2024-01-05 07:53:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var game;
 function choose(choice){
     game = choice;
+    document.getElementById("game").innerHTML = game;
 }
 
 function test(click){
     alert("You chose " + user);
 }     
+let text =
+    document.getElementById("game").innerHTML = game;
